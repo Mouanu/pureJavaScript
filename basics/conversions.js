9f8d0b796f9ed3aaf09ae5bb1a3b34b7756e2a38
mouanu <biswasanushila27@gmail.com>
2025-07-13 21:34:31 +0530

    Add conversions.js file with examples of type conversion and logging

---
+++ basics/conversions.js
@@ -0,0 +1,63 @@
+let score = 15
+
+ //console.log(typeof score) // number (small letter)
+
+//  let val = "69abc"
+//  let valueInNumber = Number(val)
+//  console.log(typeof valueInNumber) // number
+//  console.log(valueInNumber) // NAN => not a number
+
+//  let another = null
+//  console.log(typeof another) // object
+//  let num = Number(another)
+//  console.log(typeof num) // number
+//  console.log(num) // value of it is num
+
+// let another = undefined
+//  console.log(typeof another) // undefined
+//  let num = Number(another)
+//  console.log(typeof num) // number
+//  console.log(num) // NAN = Not a number
+
+
+// let another = true
+//  console.log(typeof another) // boolean
+//  let num = Number(another)
+//  console.log(typeof num) // number
+//  console.log(num) // 1
+
+
+// let another = "gnregek"
+//  console.log(typeof another) // string
+//  let num = Number(another)
+//  console.log(typeof num) // number
+//  console.log(num) // NAN = not a number
+ // type of NAN is number
+
+ // "33" => 33
+ // "33ans" =>NAN
+ // true =>1 , false => 0
+
+
+//  let isLoggedIn = 1
+//  let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn) // true
+
+
+//  let isLoggedIn = ""
+//  let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn) // false
+
+//   let isLoggedIn = "foegioernl"
+//  let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn) // true
+
+ // 1 => true; 0 => false
+ // "" => false
+ // "anushila" =>true
+
+
+ let someNumber = 33
+ let stringNumber = String(someNumber)
+ console.log(stringNumber)
+ console.log(typeof stringNumber)
