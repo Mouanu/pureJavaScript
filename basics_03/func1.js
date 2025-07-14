c8a2fa85a8222cd2109c7cc938db294818d23743
mouanu <biswasanushila27@gmail.com>
2025-07-14 22:28:43 +0530

    Refactor func1.js to uncomment example usages and improve object handling demonstration

--- basics_03/func1.js
+++ basics_03/func1.js
@@ -17,10 +17,23 @@ function login(username = "mouli"){
 }
 
 // nothing is passed to the login function
-console.log(login("arnab")) // undefined is logged in
+//console.log(login("arnab")) // undefined is logged in
 
 
+// ...num1 = rest operator
+function calcualteCartPrice(val1,val2,...num1){
+        return num1
+}
+//console.log(calcualteCartPrice(200,300,400,67,9000,3243)) // [ 400, 67, 9000, 3243 ]
 
 
-
+const user = {
+    username: "Anushila",
+    price: 199
+}
     
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+handleObject(user)
