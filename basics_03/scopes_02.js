da4294108a3c5a1182f56d785a24d0f34de649b2
mouanu <biswasanushila27@gmail.com>
2025-07-14 23:38:41 +0530

    Add scopes_02.js with examples of variable scoping and global context

---
+++ basics_03/scopes_02.js
@@ -0,0 +1,17 @@
+
+// var c = 400
+
+// scopes
+{
+    let a = 10
+    const b = 20
+   
+}
+
+// console.log(a)  // error
+// console.log(b)  // error
+console.log(c)  // 30, right
+
+
+// node er bhitore global scope is different then
+// windows er global scope theke
